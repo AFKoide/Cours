82f7e33c753f783a70e60890609b2f59661a1a2a
AFKoide <60762504+AFKoide@users.noreply.github.com>
2021-10-06 18:55:01 +0200

    Delete Informatique directory

--- Informatique/Programme Prof/Dame/clavier.h
+++
@@ -1,5 +0,0 @@
-#ifndef __CLAVIER_H__
-#define __CLAVIER_H__
-
-void keyboard(unsigned char key, int x, int y);
-#endif
--- Informatique/Programme Prof/Dame/souris.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SOURIS_H__
-#define __SOURIS_H__
-
-void mouseDrag(int xs,int ys);
-void mouse(int button, int state,int xs, int ys);
-
-#endif
