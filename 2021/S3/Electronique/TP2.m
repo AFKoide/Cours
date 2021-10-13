04fb40ae3b316b76dab677689f98158a614f667d
AFKoide <60762504+AFKoide@users.noreply.github.com>
2021-10-13 10:51:32 +0200

    Update TP2.m

--- 2021/S3/Electronique/TP2.m
+++ 2021/S3/Electronique/TP2.m
@@ -1,26 +1,36 @@
 % Init %
 clc 
+close all
 clear all
 
 %%%- Exercice 1 -%%%
-% Constante %
-R = 100;
-C = 0.1;
-L = 0.1;
-E = 10;
-
-t = linspace(0,0.01,50);
+t = linspace(0,0.01,100);
 
 % Calcul %
-I1 = ;
+syms e 
+syms i1 R1 L
+i1 = dsolve('Di1 + (R1/L)*i1 = e/L', 'i1(0) = 0');
 
-I2 = ;
+
+syms i2 R2 C
+i2 = dsolve('Di2 + (1/(R2*C))*i2 = 0', 'i2(0)=e/R2');
+
+%Constante %
+R1 = 100;
+R2 = 100;
+C = 0.1;
+L = 0.1;
+e = 10;
+
+% Substitution %
+I1 = subs(i1);
+I2 = subs(i2);
+
+I = I1+I2;
 
 % Affichage %
-figure()
-plot(t, I1)
-plot(t, I2)
-
-
+figure(1)
+plot(t,I1, 'r', t,I2, 'g', t,I, 'b')
+legend('I1', 'I2', 'I')
 
 %%%- Exercice 2 -%%%
