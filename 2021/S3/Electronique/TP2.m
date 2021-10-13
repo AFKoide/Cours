7212da7b8a3652592c7d57d92c84298ac1818310
AFKoide <60762504+AFKoide@users.noreply.github.com>
2021-10-13 09:37:13 +0200

    Update TP2.m

--- 2021/S3/Electronique/TP2.m
+++ 2021/S3/Electronique/TP2.m
@@ -1,1 +1,26 @@
-a
+% Init %
+clc 
+clear all
+
+%%%- Exercice 1 -%%%
+% Constante %
+R = 100;
+C = 0.1;
+L = 0.1;
+E = 10;
+
+t = linspace(0,0.01,50);
+
+% Calcul %
+I1 = ;
+
+I2 = ;
+
+% Affichage %
+figure()
+plot(t, I1)
+plot(t, I2)
+
+
+
+%%%- Exercice 2 -%%%
