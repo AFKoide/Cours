3bcec6217a870e9dcbeb07bc2f06deb2f5ce6687
AFKoide <60762504+AFKoide@users.noreply.github.com>
2021-10-27 09:39:26 +0200

    Create TP4.m

---
+++ 2021/S3/Electronique/TP4.m
@@ -0,0 +1,37 @@
+% Init
+clc
+clear all
+close all
+
+% Constante
+R1= 10;
+R2= 8;
+C= 100e-6;
+E= 10;
+
+% Calcul
+syms t p;
+
+% syms R1 R2 E C;
+
+Sp= (E/p)-((R1*E)/(R1+R2))*(1/(p+(1+(R1+R2)*C)));
+Ip= (E/(R1+R2))*1/(p+(1/((R1+R2)*C)));
+
+St= ilaplace(Sp, p, t);
+It= ilaplace(Ip, p, t);
+
+% Affichage
+disp('It=')
+disp(It);
+disp('St=');
+disp(St);
+
+
+t= linspace(0,0.5e-3,1000);
+
+figure(1);
+st= subs(St, t);
+plot(t, st);
+figure(2);
+it= subs(It, t);
+plot(t, it);
