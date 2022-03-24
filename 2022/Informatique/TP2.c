c3e1c46d571eb2c8981c18077326b697d2cbfd6e
AFKoide <60762504+AFKoide@users.noreply.github.com>
2022-03-24 16:11:00 +0100

    Update TP2.c

--- 2022/Informatique/TP2.c
+++ 2022/Informatique/TP2.c
@@ -1,21 +1,51 @@
 #include "stm32l1xx.h"
 
-int main() {
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB,ENABLE);
-    //Active le périphérique GPIOB
+// fonctions pour configurer et utiliser le timer 5.
+// voir apres le main
+void TIM5_Config();
+void TIM5_delay_ms(int delay);
 
-    GPIO_InitTypeDef leds_PB; //Structure pour leds
-    GPIO_StructInit(&leds_PB); //Init la structure
-    leds_PB.GPIO_Mode = GPIO_Mode_OUT; //Défini le mode
-    leds_PB.GPIO_Pin = GPIO_Pin_7; //Choisi le pin 7
-    GPIO_Init(GPIOB,&leds_PB); //Compile le programme
+int main(void)
+{
+    TIM5_Config();
+
+    /* Activer GPIOB sur AHB */
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB,ENABLE);
+    /* Configurer PB7 */
+    GPIO_InitTypeDef gpio_b;
+    GPIO_StructInit(&gpio_b);
+    gpio_b.GPIO_Mode = GPIO_Mode_OUT;
+    gpio_b.GPIO_Pin = GPIO_Pin_7;
+    GPIO_Init(GPIOB,&gpio_b);
 
     while(1) {
-        GPIO_SetBits(GPIOB,GPIO_Pin_7); //Allume la led
-        for(int k=0; k<100000;k++){} //Attente
-        GPIO_ResetBits(GPIOB,GPIO_Pin_7); //Eteind la led
-        for(int k=0; k<100000;k++){} //Attente
+        GPIO_SetBits(GPIOB,GPIO_Pin_7);
+        TIM5_delay_ms(500);
+        GPIO_ResetBits(GPIOB,GPIO_Pin_7);
+        TIM5_delay_ms(500);
     }
+}
 
-    return 0;
+// ### TIM5 pour delai
+// Configuration
+void TIM5_Config()
+{
+    /* Activer TIM5 sur APB1 */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5,ENABLE);
+    /* Configurer TIM5 : prescalaire à 1 ms, periode au maximum*/
+    
+    TIM_TimeBaseInitTypeDef DelayTimer; // Crée la structure
+    TIM_TimeBaseStructInit(&DelayTimer); // Initialise la structure
+    DelayTimer.TIM_Prescaler = 16000-1; // Défini le prescalaire
+    DelayTimer.TIM_Period = 0xFFFFFFFFU; // Taille que fera la période (pleine)
+    TIM_TimeBaseInit(TIM5,&DelayTimer); // Configure le timer
+}
+
+// Fonction Delai en millisecondes
+void TIM5_delay_ms(int delay)
+{
+    TIM_SetCounter(TIM5,0);
+    TIM_Cmd(TIM5, ENABLE); // Active le compteur
+    while(TIM_GetCounter(TIM5)<delay) {} // Attend que l'on atteigne le delai
+    TIM_Cmd(TIM5, DISABLE); // Désactive le compteur
 }
