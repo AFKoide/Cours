f6e627208b1fc67fafa0122ef02ce5c7ed597024
AFKoide <60762504+AFKoide@users.noreply.github.com>
2022-04-21 11:47:19 +0200

    Update TD.m

--- 2022/Onde/TD.m
+++ 2022/Onde/TD.m
@@ -1,9 +1,14 @@
-% Mise en Jambe
+clc
+close all
+
+u = symunit;
+
 e = 1.60217663*10^-19;
 h=6.62607015*10^-34;
 H=h/(2*pi)
 m = 9.1*10^-31;
 e0 = 8.85*10^-12;
+c = 3*10^8;
 
 a0 = (4*pi*e0/m)*(H^2/e^2)
 E0=-((m*e^4)/(8*h^2*e0^2))
@@ -13,4 +18,17 @@ for n=1:4
     a = a0*n^2
     E = -E0/(n^2)
     EE = E/e
+    if EE<13.4
+        hh1=vpa((h*c)/((13.4-EE)*e))*u.m;
+        hh1=unitConvert(hh1,u.nm)
+    end
+     if EE<3.4
+         hh2=vpa((h*c)/((3.4-EE)*e))*u.m;
+         hh2=unitConvert(hh2,u.nm)
+     end
+     if EE<1.51
+         hh3=vpa((h*c)/((1.51-EE)*e))*u.m;
+         hh3=unitConvert(hh3,u.nm)
+     end
 end
+
