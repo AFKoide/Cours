0bc8c2f322b7207f790fbeecd687b6156b2b4c90
AFKoide <60762504+AFKoide@users.noreply.github.com>
2022-04-21 11:50:33 +0200

    Update TD.m

--- 2022/Onde/TD.m
+++ 2022/Onde/TD.m
@@ -10,14 +10,14 @@ m = 9.1*10^-31;
 e0 = 8.85*10^-12;
 c = 3*10^8;
 
-a0 = (4*pi*e0/m)*(H^2/e^2)
-E0=-((m*e^4)/(8*h^2*e0^2))
+a0 = (4*pi*e0/m)*(H^2/e^2) % En Angstrom
+E0=-((m*e^4)/(8*h^2*e0^2)) % En Joule
 
 for n=1:4
     n
-    a = a0*n^2
-    E = -E0/(n^2)
-    EE = E/e
+    a = a0*n^2 % En Angstrom
+    E = -E0/(n^2) % En Joule
+    EE = E/e % En eV
     if EE<13.4
         hh1=vpa((h*c)/((13.4-EE)*e))*u.m;
         hh1=unitConvert(hh1,u.nm)
