c2b5ef199ffb0768889ef35c8e4873b9f6f408de
AFKoide <chose.neant@gmail.com>
2022-04-27 16:49:18 +0200

    Update Brouillon TP5.c

--- 2022/Informatique/Brouillon TP5.c
+++ 2022/Informatique/Brouillon TP5.c
@@ -11,11 +11,12 @@ void main()
 {
     Convertisseur = (float *) malloc( 101 * sizeof(float) ); 
     Tension = (float *) malloc( 101 * sizeof(float) );
-// Alloue à une chaine float 101 caractères avec chacun faisant la taille d'un float
+// Alloue pour une chaine type float 101 caractères de la taille d'un float.
+// 101 car on a une chaine de 100 caractères plus \0.
 
     for (int k = 0 ; k<100 ; k++)
     {
-        Convertisseur[k] = 511*sin(k*2*pi/100) + 2047;
+        Convertisseur[k] = 511.0*sin(k*2*pi/100) + 2047;
         Tension[k] = 3*x[k] / 4095;
 // 2^12 = 4096 donc la valeur max du convertisseur est 4096-1. La valeur min est 0.
 // Donc pour 3V, le convertisseur est égal à 4095. On fait un produit en croix
@@ -26,5 +27,7 @@ void main()
 //    Tension_max = 3*Convertisseur_max / 4095; // Conversion (produit en croix).
 //    Convertisseur_moy = 2047 + 0; // Valeur min est 2047 plus la valeur quand sin = 0;
 //    Tension_moy = 3*Convertisseur_moy / 4095; // Conversion (produit en croix).
+
+    
 }
 
