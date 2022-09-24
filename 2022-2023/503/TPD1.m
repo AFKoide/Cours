5199a7b9ccee11d1bae49eff6372115d535bf150
Gerier Kantique <chose.neant@gmail.com>
2022-09-24 19:45:55 +0200

    Update TPD1.m

--- 2022-2023/503/TPD1.m
+++ 2022-2023/503/TPD1.m
@@ -2,11 +2,16 @@ clear all
 close all
 clc
 
-f = 50;
 t = linspace(0,10,1000);
 
-w = 2*pi*f;
-ve = 230*sqrt(2)*sin(w.*t);
+% f = 50;
+% w = 2*pi*f;
+% ve = 230*sqrt(2)*sin(w.*t);
 
-Rf = 2900;
-Lm = 0.63;
+% Rf = 2900;
+% Lm = 0.63;
+
+syms Rf Lm w
+
+Zeq = 1/((1/Rf)+(1/(1i*Lm*w)))
+Zeq = solve(Zeq)
