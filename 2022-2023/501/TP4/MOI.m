1c1ebf7fca83a78e34ff7855a7c0feb7986c90a7
AFKoide <60762504+AFKoide@users.noreply.github.com>
2022-10-05 18:00:04 +0200

    Update MOI.m

--- 2022-2023/501/TP4/MOI.m
+++ 2022-2023/501/TP4/MOI.m
@@ -1,8 +1,47 @@
-clear all
 close all
 clc
 
-netlist={'R1 Vin 1 R';'R2 1 e+ R';'C1 1 Vout C1';'C2 e+ 0 C2';'X1 e+ Vout Vout';'V1 Vin 0 Ve'};
-[X,name]=fspice(netlist);
-H=X(3)/X(2);
-pretty(H)
+%%%- Théorie -%%%
+netlist={
+    'R1 in 1 R'
+    'R2 1 e+ R'
+    'C1 1 out C1'
+    'C2 e+ 0 C2'
+    'X1 e+ out out'
+    'V1 in 0 Ve'};
+[X,name]=fspice(netlist)
+H=X(4)/X(3);
+H
+
+syms Ca Cb
+%[Q,w0] = solve(1/(Q*w0) == 2*Ra*Ca*Cb , 1/(w0) == Ra*sqrt(Ca*Cb),w0,Q)
+w0 = 2*pi*1e4;Q=2;R=1e3;
+[Ca,Cb] = solve([1/(Q*w0) == 2*R*Cb , 1/(w0^2) == R^2*Ca*Cb],[Ca,Cb]);
+
+C2 = 3.3e-9;
+C1 = 47e-9;
+
+%%%- Pratique -%%%
+% Créer un tableau
+% Premiere colonne fréquence
+% Seconde colonne Ve
+% Troisième colonne Vs
+% save [nom] [nom fichier]
+% semilogx(m(:,1),20*log10(m(:,3)./m(:,2)),'s')
+
+
+%%% Bode
+% Gain max et fréquence pour cette valeur max
+% G = double(20*log10(abs(subs(H))))
+% [Gmax,id] = max(G)
+% fmax = ft(id)
+
+ft=logspace(2,6,1000);
+s=2*pi*ft*1i;
+semilogx(ft,20*log10(abs(subs(H))),'b')
+hold on
+% semilogx(fp,20*log10(abs(Hp)),'r')
+xlabel('Fréquence (Hz)')
+ylabel('Module (dB)')
+legend('Théorique','Pratique')
+grid on
