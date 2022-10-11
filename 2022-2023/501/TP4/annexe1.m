565d9c6814e2f3610fa74fd486c662bab8f227d5
Gerier Kantique <chose.neant@gmail.com>
2022-10-11 22:04:08 +0200

    Update annexe1.m

--- 2022-2023/501/TP4/annexe1.m
+++ 2022-2023/501/TP4/annexe1.m
@@ -1,1 +1,10 @@
-
+netlist=...
+    {'R1 Vin 1 R';
+    'R2 1 e+ R';
+    'C1 1 Vout C1';
+    'C2 e+ 0 C2';
+    'X1 e+ Vout Vout'
+    'V1 Vin 0 Ve'};
+[X,name]=fspice(netlist);
+H=X(3)/X(2);
+pretty(H)
