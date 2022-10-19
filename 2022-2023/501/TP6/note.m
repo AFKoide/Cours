0d6b8019218bbe69cf2a37ee87a53de3254b09c2
AFKoide <60762504+AFKoide@users.noreply.github.com>
2022-10-19 16:33:10 +0200

    Update note.m

--- 2022-2023/501/TP6/note.m
+++ 2022-2023/501/TP6/note.m
@@ -4,9 +4,8 @@ netlist={
     'R1 e- 0 R'
     'R2 out e- R'
     'Rl out 0 Rl'};
-[X,name]=fspice(netlist)
-Vout = X(3)
-H = X(3)/X(1)
+[X,name]=fspice(netlist) 
+H = X(3)/X(1) % Gain de 2.
 
 % Montage avec AOP permet de coller des morceaux de circuit ensemble au
 % lieu de faire bout pas bout car montage AOP a une impédance de sortie nulle 
@@ -15,4 +14,8 @@ H = X(3)/X(1)
 netlist={
     'V2 e+ 0 V2'
     'Zs e+ 0 Z'};
-[X,name]=fspice(netlist)
+[X,name]=fspice(netlist) % On voit que I=0 donc I=V/Z -> Z infini.
+
+% Emetteur commun/Collecteur commun -> Suiveur
+% Collecteur commun => impédance entrée grande - impédance sortie faible
+% Emetteur commun 
