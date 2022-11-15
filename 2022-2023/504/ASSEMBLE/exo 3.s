a7ecb82ff78c7cd23a4dca17f5f2120269f23cc4
AFKoide <60762504+AFKoide@users.noreply.github.com>
2022-11-15 16:37:18 +0100

    Create exo 3.s

---
+++ 2022-2023/504/ASSEMBLE/exo 3.s
@@ -0,0 +1,38 @@
+.equ 	N, 10	/* number of elements of the vector */
+
+.data
+
+vector: 	.int 1,236,3,154,32,11,236,153,254,35
+max: 		.int 0
+mul_by_17: 	.int 0
+
+
+.text
+.global main
+
+main:		ldr r0,=vector
+			mov r1, #0
+			mov r2, #0
+			mov r3, #0
+
+loop:		add r1, r1, #1 /* i */
+			ldr r2, [r0]
+
+			cmp r2, r3 /* < */
+			blt faux
+			mov r3, r2
+faux:		add r0, r0, #4
+			cmp r1, #10 /* Sortir LOOP */
+			blt loop
+
+
+			mov r1, #0
+			mov r4, #0
+mult:		add r1, r1, #1 /* i */
+			add r4, r3, r4
+			cmp r1, #14 /* Sortir LOOP */
+			blt mult
+
+			str
+			bx lr
+.end
