380623b8685bd0663213cb8180704c887aaae5ed
AFKoide <60762504+AFKoide@users.noreply.github.com>
2022-11-29 16:04:02 +0100

    Update exo 2.3.s

--- 2022-2023/504/ASSEMBLE/exo 2.3.s
+++ 2022-2023/504/ASSEMBLE/exo 2.3.s
@@ -1,24 +1,35 @@
 .data
-nombre:	    .byte 11
-resultat:   .bool
+nombre:	    .byte 9
+primaire:   .byte
 
+.text
 .global     main
 
-main:
-pas_beau:	mov r0, #0
+main:		mov r0, #1
+            mov r4, #1
 		    ldr r1, =nombre
             ldr r2, [r1]
-            ldr r3, =resultat
 
-loop:		add r0, r0, #1  // R0 compteur
-            udiv r2, r2, #2 // divise la valeur par deux
+			b racinebis
 
-            cmp r0, #4   // Si on a fait un bit entier.
-            beq fin
-            cmp r2, #1 // Si R2 = 0, loop.
-            ble loop
+racine:		add r4, r4, #2
+racinebis:	cmp r4, r2
+            blt racine
 
-fin:
-            str r0, [r3]
-			bx lr
+            mov r0, #1
+
+loop:		add r0, r0, #1
+            udiv r3, r2, r0 		// N/x = y
+            mls r3, r3, r0, r2		// M = N-(x*y)
+            cmp r3, #0
+            beq primair
+            cmp r0, r4
+            bne loop
+            b fin
+
+
+primair:	ldr r1, =primaire
+            str r0, [r1]
+
+fin:        bx lr
 .end
