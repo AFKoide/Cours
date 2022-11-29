0f01fab9eb26e042a43c31008198b4237b149ccd
AFKoide <60762504+AFKoide@users.noreply.github.com>
2022-11-29 16:16:57 +0100

    Update exo 2.3.s

--- 2022-2023/504/ASSEMBLE/exo 2.3.s
+++ 2022-2023/504/ASSEMBLE/exo 2.3.s
@@ -1,5 +1,5 @@
 .data
-nombre:	    .byte 9
+nombre:	    .byte 997
 primaire:   .byte
 
 .text
@@ -7,23 +7,25 @@ primaire:   .byte
 
 main:		mov r0, #1
             mov r4, #1
+            mov r5, #0
+            mov r6, #0
 		    ldr r1, =nombre
             ldr r2, [r1]
 
 			b racinebis
 
 racine:		add r4, r4, #2
-racinebis:	cmp r4, r2
+racinebis:	add r5, r5, r4
+			add r6, r6, #1
+			cmp r5, r2
             blt racine
-
-            mov r0, #1
 
 loop:		add r0, r0, #1
             udiv r3, r2, r0 		// N/x = y
             mls r3, r3, r0, r2		// M = N-(x*y)
             cmp r3, #0
             beq primair
-            cmp r0, r4
+            cmp r0, r6
             bne loop
             b fin
 
