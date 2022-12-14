d4eef8ac92a13acba0326865e671adfdf328619c
AFKoide <60762504+AFKoide@users.noreply.github.com>
2022-12-14 14:59:33 +0100

    Update Transistor.m

--- 2022-2023/501/PROJET/Transistor.m
+++ 2022-2023/501/PROJET/Transistor.m
@@ -23,8 +23,8 @@ Vb1=Vin;Vb2=-Vin;
 
 Ve2 = 0.6+Vb2;
 Ve1 = 0.6+Vb1;
-Ie1 = ((12-Ve1)/R1)./(1+exp(0.025.*Vb1);
-Ie2 = ((12-Ve2)/R1)./(1+exp(0.025.*Vb2);
+Ie1 = ((12-Ve1)/R1)./(1+exp(0.025.*Vb1));
+Ie2 = ((12-Ve2)/R1)./(1+exp(0.025.*Vb2));
 
 figure
 plot(Vin1,Ib1)
