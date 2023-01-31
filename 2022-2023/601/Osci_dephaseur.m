b840e68771d51241681d634fc2fd561f8ebbb3df
Gerier Kantique <chose.neant@gmail.com>
2023-01-31 11:47:23 +0100

    essaie

---
+++ 2022-2023/601/Osci_dephaseur.m
@@ -0,0 +1,27 @@
+clear;
+
+% R1/R = 29 <=> R1=29k et R=1k
+% C fixe frequence car R fix donc f = 1k <=> C = 6.4975e-08 ou 6.5e-8
+
+netlist={
+    uA741('e+','e-','out')
+    'R1 e- out 29e3'
+    'C1 out d1 6.5e-8'
+    'R3 0 d1 1e3'
+    'C2 d1 d2 6.5e-8'
+    'R4 0 d2 1e3'
+    'C3 d2 d3 6.5e-8'
+    'R2 d3 e- 1e3'
+    '.tran tstop=10e-3 dtmax=1e-5 initTRAN=CI'
+    '.probe out d3'
+    };
+sim=uacs(netlist)
+
+R=1e3; R1=29e3;
+C=6.5e-8;
+
+f = 1/(2*pi*R*C*sqrt(6))
+
+plot(sim.t,sim.X_t(sim.probe_id(1:2),:))
+grid
+legend('Vout','Vin')
