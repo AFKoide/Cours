9faf43611b1eaa8929443a724370a8a4caaf5b8d
AFKoide <60762504+AFKoide@users.noreply.github.com>
2023-02-09 08:33:37 +0100

    Début

---
+++ 2022-2023/605/TP1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+print("TP Algebre Lineaire!\n\n")
+
+#scalaire
+s = 5
+print("s vaut\n",s) #Affichage de s
+
+#vecteur
+v = np.array([1,2,3,4,5,5,6,7])
+print("Vecteur n\n",v)
+
+z = np.array([[0,0],[0,0]])
+print("Vecteur n\n",z)
