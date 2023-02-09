8d6290d0702bb67106fb31351e53a2b4d6b52e40
AFKoide <60762504+AFKoide@users.noreply.github.com>
2023-02-09 10:38:46 +0100

    Finito

--- 2022-2023/605/TP1_p2.py
+++ 2022-2023/605/TP1_p2.py
@@ -2,4 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from control.matlab import *
 
-H = 1/(p^2+2*p+5)
+H = tf(1,[1,2,5])
+print(H)
+
+#Determination de la réponse indicielle
+#y represente la sortie et T le vecteur temps
+y,T = step(H)
+
+plt.plot(T,y)
+plt.title("Réponse Indicielle du système")
+plt.xlabel('temps (s)');plt.ylabel('Réponse')
+plt.grid(True);plt.show()
+
+
+def FTC(systeme):
+    H = tf(1,systeme)
+    y,T = step(H)
+
+    plt.plot(T,y)
+    plt.title("Réponse Indicielle du système")
+    plt.xlabel('temps (s)');plt.ylabel('Réponse')
+    plt.grid(True);plt.show()
