f3ac8fe0b519420120fe9c4366af39837327d09d
AFKoide <60762504+AFKoide@users.noreply.github.com>
2023-02-09 10:09:29 +0100

    Ré-agencement

--- 2022-2023/605/TP1_p1.py
+++ 2022-2023/605/TP1_p1.py
@@ -25,20 +25,20 @@ print("\nProduit des matrices:\n",s*M)
 
 print("Dimension de M: ",np.shape(M))
 
-print("\nVecteur v: ",v[0:6])
+print("\nVecteur: première ",v[0]," et dernière",v[-1])
 print("Valeur à 3x2: ",M[2,1]) #Colonne:4 ; Ligne:3 -> 4*3+2
 # Il compte les lignes et colonnes à partir de 0 (0=1° ligne/colonne)
 
 
 # Diagonale
+m = np.diag([0,2,4])
 n = np.eye(4)
-print("\nDiagonale matrice identité:",np.diag(n,0))
 
 
 # Manipulation Mathématique Matrice
 A = np.arange(0,4); A.shape = (2,2)
 B = np.array([[1,1],[1,1]])
-print("A+B\n",A+B)
+print("\nA+B\n",A+B)
 print("A*B\n",A*B)
 print("AB\n",np.dot(A,B))
 print("BA\n",np.dot(B,A))
@@ -47,11 +47,29 @@ print("B\n",B.T)
 
 
 C = A+B
-print("Déterminant de C: ",np.linalg.det(C))
+print("\nDéterminant de C: ",np.linalg.det(C))
 
 F = np.array([[1,2,7],[-2,-4,-14]])
 print("Rang de F: ",np.linalg.matrix_rank(C))
 
 X = np.array([[3,1],[0,1]])
-print("Inverse de X:\n",np.linalg.inv(X)*X)
-print(X*np.linalg.inv(X))
+print("\nInverse de X:\n",np.linalg.inv(X) @ X)
+print(X @ np.linalg.inv(X))
+
+Y = np.array([[0,1],[1,1],[1,0]])
+print("Pseudoinverse de Moore:\n",Y @ np.linalg.pinv(Y))
+print(np.linalg.pinv(Y) @ Y)
+
+
+
+
+# FONCTIONS (a executer depuis la console)
+def compteur5():
+    i=0
+    while(i <= 5):
+        print(i)
+        i=i+1
+
+def cube(be):
+    cu = be*be*be
+    return cu
