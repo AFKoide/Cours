8065dfa543201a3ad81e0808969423ff10847453
AFKoide <60762504+AFKoide@users.noreply.github.com>
2023-02-09 08:59:52 +0100

    update

--- 2022-2023/605/TP1.py
+++ 2022-2023/605/TP1.py
@@ -4,11 +4,23 @@ print("TP Algebre Lineaire!\n\n")
 
 #scalaire
 s = 5
-print("s vaut\n",s) #Affichage de s
+print("s vaut : ",s) #Affichage de s
 
 #vecteur
 v = np.array([1,2,3,4,5,5,6,7])
-print("Vecteur n\n",v)
+w = np.array([[1],[2],[3],[4],[5],[6],[7]])
+print("\nVecteur\n",v)
 
-z = np.array([[0,0],[0,0]])
-print("Vecteur n\n",z)
+zeros = np.zeros((2,2))
+print("\nVecteur de zéros:\n",zeros)
+
+# Produit
+M = np.arange(1,13)
+M.shape = (3,4)
+print("\nProduit des matrices:\n",s*M)
+
+print("\nDimension de M: ",np.shape(M))
+
+print(v[0:6])
+print(M[2,1]) #Colonne:4 ; Ligne:3 -> 4*3+2
+# Il compte les lignes et colonnes à partir de 0 (0=1° ligne/colonne)
