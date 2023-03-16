1c867e0554468122aa1988c229af8cbac354e426
AFKoide <60762504+AFKoide@users.noreply.github.com>
2023-03-16 11:42:03 +0100

    Séance 2

--- 2022-2023/605/truc.py
+++ 2022-2023/605/truc.py
@@ -1,7 +1,9 @@
-from math import cos,sin
+from math import acos,cos,sin,atan2,sqrt
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+## CALCUL DE L1 L2 L0-L3 
 q11= np.radians(90) ;q21= np.radians(51) ;q31= np.radians(42) ;q41= np.radians(90)
 q12= np.radians(0)  ;q22= np.radians(65) ;q32= np.radians(21) ;q42= np.radians(45)
 x1 = 0.229          ;x2 = 0.226          ;x3 = 0.325          ;x4 = 0.181
@@ -24,3 +26,29 @@ l = a@B
 
 print("\nTest 1")
 print("l0-l4: ",l[0],"\tl1: ",l[1],"\tl2: ",l[2],"\tl3: ",l[3])
+
+l04=l[0];l1=l[1];l2=l[2];l3=l[3]
+
+
+## MODELE GEOMETRIQUE DIRECT
+q0=np.radians(90)
+q1=np.radians(51)
+q2=np.radians(65)
+q3=np.radians(0)
+
+d = l1*cos(q1)+l2*cos(q2)+l3*cos(q3)
+x = d*cos(q0)
+y = d*sin(q0)
+z = l04+l1*sin(q1)-l2*sin(q2)
+
+print("x: ",x,"\ty: ",y,"\tz: ",z)
+
+
+## MODELE GEOMETRIQUE INVERSE
+x=0.325;y=0;z=0.073
+
+q0 = atan2(x,-y)
+q1 = atan2(z+l04,x-l3)+atan2(l2,l1)
+q2 = -q1+acos(((sqrt(x**2+y**2)-l3)**2+(z-l04)**2-l1**2-l2**2)/(2*l1*l2))
+
+print("q0: ",np.degrees(q0),"\tq1: ",np.degrees(q1),"\tq2: ",np.degrees(q2))
