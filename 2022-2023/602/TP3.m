8d547aac99245935185c9830f29555f66d66081b
Gerier Kantique <chose.neant@gmail.com>
2023-03-18 14:31:57 +0100

    Create TP3.m

---
+++ 2022-2023/602/TP3.m
@@ -0,0 +1,28 @@
+clear all;
+close all;
+
+IDEL=[-50 -20 -10 0 5 10 15 20 25 30 35 40 45 50]
+V_trans=[0 0 0 74e-3 -212e-3 -625e-3 -1.02 -1.42 -1.80 -2.21 -2.60 -3 -3.40 -3.87];
+plot(IDEL,-V_trans);
+title('Evolution de la tension de sortie transimpedance en fonction du courant d alimentaion de la DEL');
+xlabel('Courant DEL (mA)');
+ylabel('Tension transimpedance (V)');
+grid;
+
+load 4b.dat
+load 4ba.dat
+load 4c.dat
+
+plot(X4b(:,1),X4b(:,2),X4ba(:,1),X4ba(:,2),'r');
+title('FFT ');
+xlabel('Fréquence (Hz)');
+ylabel('Décibel (dB)');
+legend('Sans Atténuateur','Atténuateur oc=0.6')
+grid;
+
+plot(X4c(:,1),X4c(:,2));
+title('Evolution de la tension de sortie transimpedance en fonction du courant d alimentaion de la DEL');
+xlabel('Fréquence (Hz)');
+ylabel('Décibel (dB)');
+legend('Atténuateur oc=5')
+grid;
