574648674a4ba46aca5a3edfa544bd08895768c3
AFKoide <60762504+AFKoide@users.noreply.github.com>
2023-04-20 10:09:41 +0200

    Update TP5.py

--- 2022-2023/605/TP5.py
+++ 2022-2023/605/TP5.py
@@ -15,12 +15,6 @@ H = tf (num, den)
 G = c2d(H, 1)
 print('La fonction de transfert Gd(z) vaut ', G)
 
-y,T= step(feedback(G,1))
-plt.plot(T,y)
-plt.title("Réponse Indicielle")
-plt.xlabel('temps (s)');plt.ylabel('Réponse')
-plt.grid(True);plt.show()
-
 
 
 # EXERCICE 2
@@ -31,3 +25,27 @@ Ht = tf([1],[1/4,0.707,1])
 Gtz= c2d(Ht,1)
 print('La fonction de transfert continue théorique Ht(p) vaut ',Ht)
 print('La fonction de transfert discrète théorique Gt(z) vaut ',Gtz)
+
+
+Ca = tf([1,-0.3679],[1,0.7189],1)
+Cb = tf([1,0],[1,-1],1)
+Cc = tf([5.231,-2.56],[1,0],1)
+C = Ca*Cb*Cc
+Gcorrige = feedback(C*G,1)
+
+plt.figure()
+y,T= step(C*G)
+plt.plot(T,y)
+plt.title("Réponse Indicielle - Boucle ouverte")
+plt.xlabel('temps (s)');plt.ylabel('Réponse')
+plt.grid(True);plt.show()
+
+plt.figure()
+y,T= step(Gcorrige)
+plt.plot(T,y)
+plt.title("Réponse Indicielle - Boucle fermée")
+plt.xlabel('temps (s)');plt.ylabel('Réponse')
+plt.grid(True);plt.show()
+
+# On peut observer que la réponse en boucle fermée a une erreur de position nulle et un temps de réponse extrèmement faible.
+# Cependant, on a un dépassement très important.
