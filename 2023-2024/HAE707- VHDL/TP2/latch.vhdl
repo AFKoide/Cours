094966a66ea6abbb59707fa73ffee51b4cc61f3b
AFKoide <60762504+AFKoide@users.noreply.github.com>
2023-10-06 17:10:49 +0200

    Update latch.vhdl

--- 2023-2024/HAE707- VHDL/TP2/latch.vhdl
+++ 2023-2024/HAE707- VHDL/TP2/latch.vhdl
@@ -3,11 +3,11 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.std_logic_arith.all;
 USE IEEE.std_logic_unsigned.all;
 
--- Bascule D
+-- Verrou D
 ENTITY latch IS
 	PORT (
-		e, d, reset : IN std_logic; -- Protéger contre écriture
-		q           : OUT std_logic -- Protéger contre lecture
+		d, clk, reset : IN std_logic; -- Protéger contre écriture
+		q, qb         : OUT std_logic -- Protéger contre lecture
 	);
 END latch;
 
@@ -15,15 +15,15 @@ END latch;
 ARCHITECTURE bascule_d OF latch IS
 SIGNAL temp_q : STD_LOGIC;
 BEGIN
-	latch : PROCESS (e,d,reset) BEGIN
+	copie : PROCESS (clk, reset) BEGIN
 		IF reset = '1' THEN
 			temp_q <= '0';
-		ELSIF e = '1' THEN
+		ELSIF clk = '1' THEN
 			temp_q <= d;
 		ELSE
 			temp_q <= temp_q;
 		END IF;
 	END PROCESS;
     q <= temp_q;
-    qb <= NOT(temp-q);
+    qb <= not(temp_q);
 END bascule_d;
