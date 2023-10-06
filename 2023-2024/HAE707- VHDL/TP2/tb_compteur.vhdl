04dda295df0559ae8509525b3e89f1301b348e80
AFKoide <60762504+AFKoide@users.noreply.github.com>
2023-10-06 17:30:53 +0200

    Update tb_compteur.vhdl

--- 2023-2024/HAE707- VHDL/TP2/tb_compteur.vhdl
+++ 2023-2024/HAE707- VHDL/TP2/tb_compteur.vhdl
@@ -10,53 +10,45 @@ end tb_compteur;
 
 architecture tb of tb_compteur is
     component compteur
-        port (clk   : in std_logic;
-              reset : in std_logic;
-              s     : out std_logic_vector (7 downto 0));
+        port (clk    : in std_logic;
+              reset  : in std_logic;
+              config : in std_logic;
+              s      : out std_logic_vector (7 downto 0));
     end component;
 
-    signal clk   : std_logic;
-    signal reset : std_logic;
-    signal s     : std_logic_vector (7 downto 0);
+    signal clk    : std_logic;
+    signal reset  : std_logic;
+    signal config : std_logic;
+    signal s      : std_logic_vector (7 downto 0);
 
-    constant TbPeriod : time := 1000 ns; -- EDIT Put right period here
+    constant TbPeriod : time := 10 ns; -- EDIT Put right period here
     signal TbClock : std_logic := '0';
     signal TbSimEnded : std_logic := '0';
 
 begin
     dut : compteur
-    port map (clk, reset, s);
+    port map (clk, reset, config, s);
 
     -- Clock generation
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
-
     -- EDIT: Check that clk is really your main clock signal
     clk <= TbClock;
 
-    stimuli : process
+    Horloge : process
     begin
-        -- EDIT Adapt initialization as needed
-
-        -- Reset generation
-        -- EDIT: Check that reset is really your reset signal
-        reset <= '1';
-        wait for 100 ns;
-        reset <= '0';
-        wait for 100 ns;
-
-        -- EDIT Add stimuli here
         wait for 100 * TbPeriod;
-
-        -- Stop the clock and hence terminate the simulation
         TbSimEnded <= '1';
         wait;
     end process;
-
+    Entree : process
+    begin
+        reset <= '1';
+        wait for 1ns;
+        reset <= '0';
+        config <= '0';
+        wait for 20 * TbPeriod;
+        reset <= '1';
+        wait for TbPeriod;
+        config <= '1';
+    end process;
 end tb;
-
--- Configuration block below is required by some simulators. Usually no need to edit.
-
-configuration cfg_tb_compteur of tb_compteur is
-    for tb
-    end for;
-end cfg_tb_compteur;
