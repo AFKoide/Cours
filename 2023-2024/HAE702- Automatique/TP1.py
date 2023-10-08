16c9f95fa24907b43be1468b4c81fdb84f5b6671
Gerier Kantique <chose.neant@gmail.com>
2023-10-08 19:32:13 +0200

    Update TP1.py

--- 2023-2024/HAE702- Automatique/TP1.py
+++ 2023-2024/HAE702- Automatique/TP1.py
@@ -1,3 +1,5 @@
+plt.close('all')
+
 #%% Introduction
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,10 +27,6 @@ from control.matlab import *
 
 
 # Question 3
-if __name__ == '__main__':
-    plt.close('all')
-    MCC()
-
 def MCC():
     R=1.52;L=2.2e-3;Ke=0.127;Km=Ke;Fv=5.06e-5;J=8.3e-5
     
@@ -40,13 +38,13 @@ def MCC():
     print(sys_ss)
     y,T = step(sys_ss)
     plt.figure()
-    plt.plot(T,y);plt.grid('on');plt.title("Angle du Moteur");plt.show()
+    plt.plot(T,y);plt.grid('on');plt.title("Angle du Moteur")
     
     C = [[0,1,0]] 
     sys_ss = ss(A,B,C,D)
     y,T = step(sys_ss)
     plt.figure()
-    plt.plot(T,y);plt.grid('on');plt.title("Vitesse Angulaire");plt.show()
+    plt.plot(T,y);plt.grid('on');plt.title("Vitesse Angulaire")
     sys_tf = tf(sys_ss)
     print(sys_tf)
     "Question 5: on observe que la fonction de transfert du moteur qui relie"
@@ -62,41 +60,72 @@ def MCC():
     
     
 
+if __name__ == '__main__':
+    plt.close('all')
+    MCC()
+
 #%% Simulation des systèmes numériques
 import numpy as np
 import matplotlib.pyplot as plt
 from control.matlab import *
 
-plt.close('all')
+print("Partie 3: Simulation des systèmes numériques\n\n")
 
 "Question 6: Les grandeurs à connaitre pour résoudre l'équation (6) sont y(k) pour k=0, 1 et 2"
 
+# Question 7 & 8
 def equ2(N):
     y = np.zeros(N+2)
     u = np.ones(N)
     for k in range(N):
-        y[k+2] = u[k] - (0.7*y[k]-0.1*y[k+1])
-    plt.plot(range(N+2),y);plt.grid("on");plt.show()
+        y[k+2] = u[k] - (0.7*y[k] - 0.1*y[k+1])
+    plt.plot(y);plt.grid("on");plt.title("Equation Récurrente")
 
-
+# Question 9
 def equtf():
     num = [1]
     den = [1,-0.1,0.7]
-    H = tf(num,den)
-    print(H)
-    y,T = step(H,linspace(0,50,500))
-    plt.figure();plt.plot(T.T,y.T);plt.grid("on");plt.show()
-    
+    H = tf(num,den,1)
+#    print('Fonction de Transfert:',H)
+    y,T = step(H)
+    plt.figure();
+    plt.plot(T.T,y.T);plt.grid("on");plt.title("TF Discrète")
+    return H
 
+# Question 10
 def equvec():
-    A = [[-R/L,-Ke/L,0],[Km/J,-Fv/J,0],[0,1,0]]
-    B = [[1/L],[0],[0]]
-    C = [[0,0,1]] 
-    D = [[0]]
-    sys_ss = ss(A,B,C,D)
-    y,T = step(sys_ss)
+    A = [[0,1],[-0.7,0.1]]
+    B = [[0],[1]]
+    C = [[-0.7,0.1]]
+    D = [[1]]
+    R1 = ss(A,B,C,D)
+#    print("Equation d'état:",R1)
+    y,T = step(R1)
     plt.figure()
-    plt.plot(T,y);plt.grid('on');plt.title("");plt.show()
+    plt.plot(T,y);plt.grid('on');plt.title("Représentation d'état R1")
+    return R1
 
-equ2(50)
-equtf()
+
+# Appel des fonctions
+equ2(100)
+H = equtf()
+R1 = equvec()
+
+# Question 11
+TF = ss2tf(R1)
+print("Fonction de Transfert issue de la Représentation d'état R1:",TF)
+
+"On peut observer que la fonction de transfert issue de la représentation d'état est différente de la fonction de transfert H."
+
+# Question 12
+R2 = tf2ss(H)
+print("Représentation d'état R2 issue de la Fonction de Transfert :\n",R2)
+
+y1, T1 = step(R1,140)
+y2, T2 = step(R2,140)
+
+plt.figure()
+plt.plot(T1,y1);plt.plot(T2,y2)
+plt.grid('on');plt.title("Comparaison de R1 et R2");plt.legend(["R1","R2"]);plt.show()
+
+# %%
