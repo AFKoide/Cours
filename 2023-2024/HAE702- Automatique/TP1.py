81b56f8c5c067cddb76bfd95e32da007611d2e4a
AFKoide <60762504+AFKoide@users.noreply.github.com>
2023-10-12 12:45:34 +0200

    Update TP1.py

--- 2023-2024/HAE702- Automatique/TP1.py
+++ 2023-2024/HAE702- Automatique/TP1.py
@@ -1,129 +1,122 @@
+"GOOGLE COLAB : mettre au début du code : !pip install control"
+"Ca permet d'installer le control."
+
 #%% Introduction
 import numpy as np
 import matplotlib.pyplot as plt
 from control.matlab import *
 plt.close('all')
 
-#fonction de transfert H(s)=$1/(s^2+2s+3)$
-num = [1.0]
-den = [1.0,2.0,3.0]
-H = tf(num,den)
-print("La fonction de transfert H est egale a:",H)
-#evaluation de la reponse indicielle
-#y represente la sortie et T le vecteur temps
-y,T = step(H,linspace(0,5,500))
-#affichage du resultat de la reponse indicielle
-plt.figure()
-plt.plot(T.T,y.T)
-plt.show()
-
-#%% Simulation d’un système continu
-import numpy as np
-import matplotlib.pyplot as plt
-from control.matlab import *
-"Question 1: i(t) est le courant consommé par le moteur (entrée), w(t) est la vitesse de rotation du moteur (sortie) et teta(t) est l'angle de sortie du moteur (sortie)."
-"Ces trois variables sont très importante pour la "
-
-
-# Question 3
-def MCC():
-    R=1.52;L=2.2e-3;Ke=0.127;Km=Ke;Fv=5.06e-5;J=8.3e-5
-    
-    A = [[-R/L,-Ke/L,0],[Km/J,-Fv/J,0],[0,1,0]]
-    B = [[1/L],[0],[0]]
-    C = [[0,0,1]] 
-    D = [[0]]
+def MCC(R,L,Ke,Km,Fv,J):
+    A = np.array([[-R/L,-Ke/L,0],[Km/J,-Fv/J,0],[0,1,0]])
+    B = np.array([[1/L],[0],[0]])
+    C = np.array([[0,0,1]])
+    D = np.array([[0]])
     sys_ss = ss(A,B,C,D)
     print(sys_ss)
+    
     y,T = step(sys_ss)
-    plt.figure()
+    plt.figure();plt.xlabel("Le système est instable.")
     plt.plot(T,y);plt.grid('on');plt.title("Angle du Moteur")
+    "QUESTION 1: La matrice C est égale à [0,0,1] et D = 0"
     
-    C = [[0,1,0]] 
-    sys_ss = ss(A,B,C,D)
-    y,T = step(sys_ss)
-    plt.figure()
-    plt.plot(T,y);plt.grid('on');plt.title("Vitesse Angulaire")
-    sys_tf = tf(sys_ss)
-    print(sys_tf)
-    "Question 5: on observe que la fonction de transfert du moteur qui relie"
-    "u(t) et w(t) est du second ordre."    
+    H = ss2tf(sys_ss)
+    print("La fonction de transfert du moteur est",H)
+    poles = pole(H)
+    print("Les pôles de H sont:",poles)
+    "QUESTION 4: un des pôle de H est nul, donc le système instable car pas strictement négatif."
     
-    C = [[1,0,0]] 
-    sys_ss = ss(A,B,C,D)
-    y,T = step(sys_ss)
-    plt.figure()
-    plt.plot(T,y);plt.grid('on');plt.title("Courant");plt.show()
-    "Question 4: Au démarrage, le moteur a besoin de beaucoup de puissance pour passer"
-    "d'un état statique à rotationnel, d'où le pic de courant."
     
+    commandabilite = ctrb(A,B)
+    print("La commandabilité est:",np.linalg.det(commandabilite),"le système est donc commandable car != 0")
+    
+    
+    K = place(A,B,[-10,-20,-100])
+    print(K)
+
+    Ac = [[-R/L,-Ke/L,0],[Km/J,-Fv/J,0],[0,1,0]]
+    Bc = [[1/L],[0],[0]]
+    Cc = [[0,0,1]] 
+    Dc = [[1]]
+    sysc_ss = ss(A-B@K,B,C-D@K,D)
+    print(sysc_ss)
+    y,T = step(sysc_ss)
+    plt.figure();plt.xlabel("Avec le correcteur, le système est stable mais a une importante erreur de position.")
+    plt.plot(T,y);plt.grid('on');plt.title("Angle du Moteur")
+
+    k1 = 1
+    Achapeau = np.block([[A,np.zeros((3, 1))],[-C,np.zeros((1, 1))]])
+    Bchapeau = np.block([[B],[0]])
+    Kchapeau = np.block([K,-k1])
+    Cchapeau = np.block([[C,0]])
+
+    syscchapeau_ss = ss(Achapeau-Bchapeau@Kchapeau,np.array([[0],[0],[0],[1]]),Cchapeau,D)
+    print(syscchapeau_ss)
+    y,T = step(syscchapeau_ss)
+    plt.figure();plt.xlabel("Le système est plus lent qu'avec seulement le correcteur P, mais l'erreur de position est nulle")
+    plt.plot(T,y);plt.grid('on');plt.title("Angle du Moteur")
     
 
 if __name__ == '__main__':
     plt.close('all')
-    MCC()
+    sys_ss = MCC(1,4e-3,0.1,0.1,3e-3,2e-3)
 
-#%% Simulation des systèmes numériques
+
+#%% Commande lineaire quadratique d’une voiture controĺee en acceleration
 import numpy as np
 import matplotlib.pyplot as plt
 from control.matlab import *
+plt.close('all')
 
-print("Partie 3: Simulation des systèmes numériques\n\n")
+A = np.array([[0,1],[0,0]])
+B = np.array([[0],[1]])
+C = np.array([[1,0]])
+D = np.array([0])
 
-"Question 6: Les grandeurs à connaitre pour résoudre l'équation (6) sont y(k) pour k=0, 1 et 2"
+sys_ss = ss(A,B,C,D)
 
-# Question 7 & 8
-def equ2(N):
-    y = np.zeros(N+2)
-    u = np.ones(N)
-    for k in range(N):
-        y[k+2] = u[k] - (0.7*y[k] - 0.1*y[k+1])
-    plt.plot(y);plt.grid("on");plt.title("Equation Récurrente")
+H = ss2tf(sys_ss)
+pole = pole(H)
+print("La fonction de transfert du système est:",H,"Et ses pôles sont",pole)
+"QUESTION 10: les pôles de H sont nuls. Le système est donc instable."
 
-# Question 9
-def equtf():
-    num = [1]
-    den = [1,-0.1,0.7]
-    H = tf(num,den,1)
-#    print('Fonction de Transfert:',H)
-    y,T = step(H)
-    plt.figure()
-    plt.plot(T.T,y.T);plt.grid("on");plt.title("TF Discrète")
-    return H
-
-# Question 10
-def equvec():
-    A = [[0,1],[-0.7,0.1]]
-    B = [[0],[1]]
-    C = [[0,1]]
-    D = [[0]]
-    R1 = ss(A,B,C,D,1)
-#    print("Equation d'état:",R1)
-    y,T = step(R1)
-    plt.figure()
-    plt.plot(T,y);plt.grid('on');plt.title("Représentation d'état R1")
-    return R1
-
-
-# Appel des fonctions
-equ2(40)
-H = equtf()
-R1 = equvec()
-
-# Question 11
-TF = ss2tf(R1)
-print("Fonction de Transfert issue de la Représentation d'état R1:",TF)
-
-"On peut observer que la fonction de transfert issue de la représentation d'état est différente de la fonction de transfert H."
-
-# Question 12
-R2 = tf2ss(H)
-print("Représentation d'état R2 issue de la Fonction de Transfert :\n",R2)
-
-y1, T1 = step(R1,40)
-y2, T2 = step(R2,40)
-
+y,T = step(sys_ss)
 plt.figure()
-plt.plot(T1,y1);plt.plot(T2,y2)
-plt.grid('on');plt.title("Comparaison de R1 et R2");plt.legend(["R1","R2"]);plt.show()
-# %%
+plt.plot(T,y);plt.grid('on');plt.title("Angle du Moteur")
+
+CO = ctrb(A,B)
+print("La commandabilité est:",np.linalg.det(CO))
+"QUESTION 12: La voiture est commandable puisque le déterminant de CO n'est pas nul."
+
+
+
+def dessinePosVelAcc(A,B,C,D):
+    voitP = ss2tf(A,B,C,D)
+    C = np.array([0,1])
+    voitV = ss2tf(A,B,C,D)
+    pos,T = step(voitP)
+    vel,T = step(voitV)
+    acc,T = impulse(voitV)
+    plt.figure()
+    plt.plot(T,pos)
+    plt.plot(T,vel)
+    plt.plot(T,acc)
+    plt.legend(['pos(t)','vel(t)','acc(t)'])
+    plt.xlabel("temps")
+    plt.grid('on');plt.show()
+
+Q = np.eye(2); R = np.eye(1)
+K, S, E = lqr(A, B, Q, R)
+
+dessinePosVelAcc(A-B@K,B,C-D@K,D)
+"Observation de la courbe: le système doit se mettre en mouvement donc l'acceleration est importante au début."
+"Quand le système accélère, la vélocité augmente et la position également, et l'accélération diminue."
+"Quand on approche de la valeur finale prévue, l'accélération devient négative : on freine."
+"Freiner fait diminuer la vitesse, et donc la variation de la position. A la valeur de finale de la position demandée, la vitesse et l'accélération sont nulles."
+
+
+# K1 = 1 * B.T @ S
+K = place(A,B,E)
+dessinePosVelAcc(A-B@K,B,C-D@K,D)
+"QUESTION 14: En utilisant la fonction place, on obtient les mêmes courbes :"
+"La fonction lqr renvoie les valeurs propres du système, que l'on utilise pour déterminer K."
